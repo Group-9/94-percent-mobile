bcd04a4118422838c94b91b43acc3d5ac47d41b1
Eoin Houlihan <ehoulih@tcd.ie>
2016-03-28 11:36:22 +0100

    Cleanup creation form

--- www/js/infoCtrl.js
+++ www/js/infoCtrl.js
@@ -1,17 +1,22 @@
 angular.module('quizApp.controllers')
- .controller('infoCtrl', function($scope, $http, $location, config) {
+.controller('infoCtrl', function($scope, $http, $location, config) {
+  $scope.gender = "0";
+  $scope.education = "0";
+  $scope.employment_status = "0";
+
  	$scope.submit = function(age, gender, education, employment_status) {
 		$http.post(config.BACKEND_URL + '/createUser', {
 			age: age,
 			gender: gender,
 			education: education,
 			employment_status: employment_status
-		}).then(function(resp){
+    })
+    .then(function(resp){
 			console.log(resp.data.id);
 			$scope.userID = resp.data.id;
 		}, function(resp) {
 
-		});
+    });
 		$location.path('level');
- 	}
+  };
 });
