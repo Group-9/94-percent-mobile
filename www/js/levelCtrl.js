82e344dce2aae4d42297cf2d75e360dfbe635fcb
Eoin Houlihan <ehoulih@tcd.ie>
2016-03-28 10:48:33 +0100

    Fix back button going back to create user screen on actual Android

--- www/js/levelCtrl.js
+++ www/js/levelCtrl.js
@@ -1,5 +1,6 @@
 angular.module('quizApp.controllers')
-.controller('LevelCtrl', function($scope, $http, config) {
+.controller('LevelCtrl', function($scope, $http, $ionicHistory, config) {
+  $ionicHistory.clearHistory();
   $http.get(config.BACKEND_URL + '/level').then(function(resp) {
     $scope.levels = resp.data;
   });
